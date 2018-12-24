2d11b7235f58cd6e5f308a3b6cf75dff9b757aa8
Ryan Price <ryapric@gmail.com>
2018-12-24 14:52:58 -0600

    Change default args and handle them in givename()

--- src/PkgTo1Point0.jl
+++ src/PkgTo1Point0.jl
@@ -1,13 +1,3 @@
-"""
-This is a main module file. In it, you should:
-
-- `export` the desired functions from your `include()` calls (comma-delimited)
-
-- `include()` your `srcfile`s. There is a stubbed, vectorized `include()` here,
-  but if you want to change it, just construct a manual array of source file
-  names.
-"""
-
 module PkgTo1Point0
 
 export migrate
